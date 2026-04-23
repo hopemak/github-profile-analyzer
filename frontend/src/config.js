7c4567885b356f98f1b71867ad3819336d10547d
hopemak <anegash880@gmail.com>
2026-04-23 06:41:41 -0700

    Fix ESLint warning in config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000';
-export default { API_URL };
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  API_URL: process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000'
+};
