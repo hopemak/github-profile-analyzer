e0c8615d8dfdfbcad4fea6804ff443d998443c52
hopemak <anegash880@gmail.com>
2026-04-24 07:44:29 -0700

    Add Personal AI Coach feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import AISummary from './components/AISummary';
 import AICoach from "./components/AICoach";
 import AICoach from "./components/AICoach";
 import AICoach from "./components/AICoach";
+import AICoach from "./components/AICoach";
 import CommitChart from './components/CommitChart';
 import Compare from './pages/Compare';
 import Trending from './pages/Trending';
@@ -64,6 +65,7 @@ function Home() {
             </div>
             <CommitChart username={user.login} />
             <AISummary username={user.login} />
+            <AICoach username={user.login} />
             <AICoach username={user.login} />
             <AICoach username={user.login} />
             <AICoach username={user.login} />
